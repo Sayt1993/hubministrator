0d714a0edc7e7fded035d69b03c37f46e766fbf8
Sayt1993 <l.giacomini1993@gmail.com>
2018-04-17 18:00:24 +0000

    fix

--- app/admin/views.py
+++ app/admin/views.py
@@ -6,17 +6,17 @@ from flask_login import current_user, login_required, login_user, logout_user
 from . import admin
 from forms import CondoDetailsForm
 from .. import db
-from ..models import Condo, Condos_Data, User
+from ..models import Condo, Condos_Data, User, Administrator_Registry
 
 #LIST
 
 @admin.route('/condominiums', methods=['GET', 'POST'])
 @login_required
 def condominiums_list():
-    
+
     admin_check = check_admin()
     condos_data = get_condo_data()
-    
+
     return render_template('admin/condominiums.html', condos_data=condos_data, title='Condominiums List')
 
 #SELECT
@@ -24,11 +24,11 @@ def condominiums_list():
 @admin.route('/condominiums/view/<int:id>', methods=['GET', 'POST'])
 @login_required
 def condominium_view(id):
-    
+
     admin_check = check_admin()
     condos = get_condo_data()
     is_allowed = check_condo_for_admin(id)
-    
+
     condo_data = Condos_Data.query.filter_by(id_condo=id).first()
 
     if(is_allowed):
@@ -37,21 +37,21 @@ def condominium_view(id):
         return render_template('errors/404.html', title='Page not found')
 
 #UPDATE
-    
+
 @admin.route('/condominiums/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def condominium_edit(id):
-    
+
     admin_check = check_admin()
     condos = get_condo_data()
     is_allowed = check_condo_for_admin(id)
-    
+
     condos_data = Condos_Data.query.get_or_404(id)
-    
+
     form = CondoDetailsForm(obj = condos_data)
-    
+
     if form.validate_on_submit():
-        
+
         condos_data.condo_name = form.condo_name.data,
         condos_data.condo_age = form.condo_age.data,
         condos_data.cf_c = form.cf_c.data,
@@ -66,33 +66,34 @@ def condominium_edit(id):
 
         db.session.commit()
         flash('The condominium details have been succesfully modified')
-    
+
     if(is_allowed):
         return render_template('admin/condominium_edit.html', form=form, title='Condominium edit')
     else:
         return render_template('errors/404.html', title='Page not found')
 
 #INSERT
-    
+
 @admin.route('/condominiums/add', methods=['GET', 'POST'])
 @login_required
 def condominium_add():
-    
+
     admin_check = check_admin()
     form = CondoDetailsForm()
 
     if form.validate_on_submit():
 
+        admin = Administrator_Registry.query.filter_by(id_usr=current_user.id).first()
+
         condo = Condo(
             condo_name = form.condo_name.data,
-            id_admin = current_user.id
+            id_admin = admin.id_admin
         )
 
         db.session.add(condo)
         db.session.commit()
 
         condo_fk = Condo.query.filter_by(condo_name=form.condo_name.data, id_admin = current_user.id).first()
-
         condo_data = Condos_Data(
             condo_name = form.condo_name.data,
             condo_age = form.condo_age.data,
@@ -119,21 +120,21 @@ def condominium_add():
         return render_template('admin/condominium_add.html', form=form, title='Condominium list')
     else:
         return render_template('errors/404.html', title='Page not found')
-            
-    
+
+
 #funzione per delete
 
-    
+
 def check_condo_for_admin(id):
-    
+
     allowed = False
     condos = get_condo_data()
-    
+
     for condo in condos:
         if(condo.id_condo == id):
             allowed = True
             break
-            
+
     return allowed
 
 def check_admin():
@@ -142,23 +143,23 @@ def check_admin():
         return True
     else:
         return render_template('errors/404.html', title='Page not found')
-    
-    
+
+
 def get_condo_data():
-    
+
     ids_condos = []
     condos_data = []
-    
+
     id_admin = current_user.id
     condos = Condo.query.filter_by(id_admin=id_admin).all()
-    
+
     for condo in condos:
         ids_condos.append(condo.id_condo)
 
     for id_condo in ids_condos:
         condo_data = Condos_Data.query.filter_by(id_condo=id_condo).first()
         condos_data.append(condo_data)
-        
+
     return condos_data
 
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,30 +1,30 @@
 # app/auth/views.py
 
 from flask import flash, redirect, render_template, url_for
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 
 from . import auth
 from forms import LoginForm, RegistrationForm
 from .. import db
-from ..models import User
+from ..models import User, Administrator_Registry
 
 @auth.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    
+
     category = form.category.data
-    
+
     is_admin = False
     is_resident = False
     is_provider = False
-    
+
     if category == "admin":
         is_admin = True
     elif category == "resident":
         is_resident = True
     elif category == "provider":
         is_provider = True
-        
+
     if form.validate_on_submit():
         user = User(
             username = form.username.data,
@@ -33,11 +33,21 @@ def register():
             is_admin = is_admin,
             is_resident = is_resident,
             is_provider = is_provider)
-        
+
         db.session.add(user)
         db.session.commit()
+
+        administrator_registry = Administrator_Registry(
+            id_usr = User.query.filter_by(email=user.email).first().id,
+            license_n = "prova",
+            p_iva = "prova",
+            condo_own_n = 11
+        )
+        db.session.add(administrator_registry)
+        db.session.commit()
+
         flash('You have successfully registered! You may now login.')
-    
+
     return render_template("auth/register.html", form=form, title='Register')
 
 
@@ -51,7 +61,7 @@ def login():
             return redirect(url_for("home.dashboard"))
         else:
             flash('Invalid email or password.')
-    
+
     return render_template("auth/login.html", form=form, title='Login')
 
 
@@ -60,5 +70,5 @@ def login():
 def logout():
     logout_user()
     flash('You have successfully been logged out.')
-    
+
     return redirect(url_for("auth.login"))
